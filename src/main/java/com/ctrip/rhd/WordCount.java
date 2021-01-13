db512037084401d9ab328761de24668d8f8100af
haodongru <1103166260@qq.com>
2021-01-13 23:23:07 +0800

    加注释

--- src/main/java/com/ctrip/rhd/WordCount.java
+++ src/main/java/com/ctrip/rhd/WordCount.java
@@ -7,6 +7,7 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
+//doubi
 // 批处理wordcount
 public class WordCount {
     public static void main(String[] args) throws Exception {
